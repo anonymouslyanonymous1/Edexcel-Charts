013a4ce258ed3db31fbff0cf5c623edce38ff5ae
Anonymously Anonymous <99310000+anonymouslyanonymous1@users.noreply.github.com>
2026-06-12 16:46:35 +0600

    Reducing filesize
    Was going through plotly docs as FOT on Vercel was reaching limits quickly, realised I was passing the whole library as response when I could just use a CDN

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ def graph():
     y_marks = [item[1] for item in data]
     hover_text = [f"{label}: {mark}" for label, mark in data]
 
-    # Create the plot
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(
@@ -70,24 +69,20 @@ def graph():
         line=dict(color='#c0fa00', width=2)
     ))
 
-    # Layout settings
     fig.update_layout(
         title=f"Goal: {grade} for {unit}",
         xaxis_title="Year",
         yaxis_title=f"Minimum Marks for {grade}",
         hovermode="x unified",
         xaxis_tickangle=-45,
-        plot_bgcolor='black',  # Set plot background to black
-        paper_bgcolor='black',  # Set paper (overall plot area) background to black
-        font=dict(color='white'),  # Set font color to white for visibility
-        xaxis=dict(showgrid=True, gridcolor='gray'),  # Gridlines color (gray for visibility)
-        yaxis=dict(showgrid=True, gridcolor='gray')   # Gridlines color (gray for visibility)
+        plot_bgcolor='black',  
+        paper_bgcolor='black',  
+        font=dict(color='white'),
+        xaxis=dict(showgrid=True, gridcolor='gray'), 
+        yaxis=dict(showgrid=True, gridcolor='gray')  
     )
 
-    # Export to HTML
-    fig.write_html("/tmp/graph.html")
-    file = open("/tmp/graph.html", "r", encoding="utf-8")
-    html = file.read()
+    html = fig.to_html(full_html=True, include_plotlyjs="cdn")
     html = re.sub("<body>", '<body style="background-color:black;">', html)
     html = re.sub('<head><meta charset="utf-8" /></head>', f'<head><meta charset="utf-8" /><title>{unit}</title> \n <link rel="icon" href="../static/icon.svg" type="image/svg+xml"></head>', html)
     return html, 200, {'Content-Type': 'text/html'}
